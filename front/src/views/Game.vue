19a797a7aba01e708dfb23f72422b10e8787987a
ImanSafae <iman-104@hotmail.fr>
2024-06-06 15:44:05 +0200

    update

--- front/src/views/Game.vue
+++ front/src/views/Game.vue
@@ -13,18 +13,20 @@
 
     onMounted(() => {
         window.addEventListener('keydown', (e) => {
-            if (e.key === 'ArrowRight') {
-                offset.value = moveRight(currentTetromino.value, gridArray, width, offset.value);
-            }
-            else if (e.key === 'ArrowLeft') {
-                currentTetromino.value = moveLeft(currentTetromino.value, gridArray, width, offset);
-            }
-            // else if (e.key === 'ArrowDown') {
-            //     currentTetromino.value = moveDown(currentTetromino.value, gridArray, width, offset);
-            // }
-            else if (e.key === 'ArrowUp') {
-                console.log("arrow up");
-                currentTetromino.value = rotate(currentTetromino.value, tetrominosArray[randomIndex.value], currentRotationIndex, gridArray, offset)
+            if (isMoveLegal(e.key, currentTetromino.value, offset.value, width, gridArray.value, tetrominosArray[randomIndex.value], currentRotationIndex.value)) {
+                if (e.key === 'ArrowRight') {
+                    offset.value = moveRight(currentTetromino.value, gridArray, width, offset.value);
+                }
+                else if (e.key === 'ArrowLeft') {
+                    offset.value = moveLeft(currentTetromino.value, gridArray, width, offset.value);
+                }
+                else if (e.key === 'ArrowUp') {
+                    currentTetromino.value = rotate(currentTetromino.value, tetrominosArray[randomIndex.value], currentRotationIndex, gridArray, offset.value)
+                }
+                else if (e.key === 'ArrowDown') {
+                    
+                    offset.value = moveDown(currentTetromino.value, gridArray, width, offset.value);
+                }
             }
         });
     });
@@ -95,6 +97,32 @@
     let currentTetromino = ref(tetrominosArray[randomIndex.value][currentRotationIndex.value]);
     console.log("current tetromino position:", currentTetromino.value);
 
+    function isMoveLegal(eventKey, tetromino, offset, width, gridArray, tetrominoRotations, currentRotationIndex) {
+        if ((eventKey === 'ArrowRight') && (isAtRightEdge(tetromino, width, offset))) {
+            return false; 
+        }
+        else if ((eventKey === 'ArrowLeft') && (isAtLeftEdge(tetromino, width, offset))) {
+            return false;
+        }
+        else if ((eventKey === 'ArrowDown') && (isAtBottom(tetromino, gridArray, width, offset))) {
+            return false;
+        }
+        else if ((eventKey === 'ArrowUp') && (isNearTetromino(tetrominoRotations, currentRotationIndex, gridArray, offset))) {
+            return false;
+        }
+        return true;
+    }
+
+    function canRotate(tetrominoRotations, currentRotationIndex, gridArray, offset) {
+        if (isNearTetromino(tetrominoRotations, currentRotationIndex, gridArray, offset))
+            return false;
+        currentRotationIndex++;
+        let newTetromino = tetrominoRotations[currentRotationIndex];
+        if (newTetromino.some((index) => ) {
+
+        }
+    }
+
     function undrawTetromino(tetromino, gridArray, offset) {
         let newGridArray = [...gridArray];
         tetromino.forEach((index) => {
@@ -111,8 +139,17 @@
         return newGridArray;
     }
 
+    function isNearTetromino(tetrominoRotations, currentRotationIndex, gridArray, offset) {
+        currentRotationIndex++;
+        if (currentRotationIndex >= tetrominoRotations.length)
+            currentRotationIndex = 0;
+        let nextRotation = tetrominoRotations[currentRotationIndex];
+        if (nextRotation.some((index) => gridArray[index + offset] === 'taken'))
+            return true;
+        return false;
+    }
+
     function isAtBottom(tetromino, gridArray, width, offset) {
-        console.log("offset to check:", offset);
         if (tetromino.some((index) => (index + width + offset) > gridArray.length)) {
             return true;
         }
@@ -124,10 +161,10 @@
     }
 
       function isAtRightEdge(tetromino, width, offset) {
-         if (tetromino.some((index) => (index + offset + 1) % width === 0)) {
-             return true;
-         }
-         return false;
+        if (tetromino.some((index) => (index + offset + 1) % width === 0)) {
+            return true;
+        }
+        return false;
     }
 
     function isAtLeftEdge(tetromino, width, offset) {
@@ -145,14 +182,12 @@
     }
 
     function rotate(tetromino, tetrominoRotations, currentRotationIndex, grid, offset) {
-        grid.value = undrawTetromino(tetromino, grid.value, offset)
-        console.log("current tetromino:", tetromino, "and current rotation index:", currentRotationIndex.value);
+        grid.value = undrawTetromino(tetromino, grid.value, offset);
         currentRotationIndex.value++;
-        if (currentRotationIndex.value === tetrominoRotations.length)
+        if (currentRotationIndex.value >= tetrominoRotations.length)
             currentRotationIndex.value = 0;
         let newTetromino = tetrominoRotations[currentRotationIndex.value];
         grid.value = drawTetromino(newTetromino, grid.value, offset)
-        console.log("new rotation index:", currentRotationIndex.value, " and new tetromino:", newTetromino);
         return newTetromino;
     }
 
@@ -162,35 +197,28 @@
         }
         grid.value = undrawTetromino(tetromino, grid.value, offset);
         offset += 1;
-        // let newTetromino = tetromino.map(index => index + 1);
         grid.value = drawTetromino(tetromino, grid.value, offset);
         return offset;
     }
 
     function moveLeft(tetromino, grid, width, offset) {
         if (isAtLeftEdge(tetromino, width, offset)) {
-            return tetromino;
+            return offset;
         }
         grid.value = undrawTetromino(tetromino, grid.value, offset);
-        let newTetromino = tetromino.map(index => index - 1);
-        grid.value = drawTetromino(newTetromino, grid.value, offset);
-        return newTetromino;
+        offset -= 1;
+        grid.value = drawTetromino(tetromino, grid.value, offset);
+        return offset;
     }
 
-    function nextTetrominoIndex(tetromino, grid, width, offset, randomIndex) {
-        // if (isAtBottom(tetromino, grid.value, width, offset)) {
-            console.log("current tetromino;", randomIndex);
-            randomIndex = Math.floor(Math.random() * tetrominosArray.length);
-            console.log("new tetromino:", randomIndex);
-        // }
+    function nextTetrominoIndex() {
+        let randomIndex = Math.floor(Math.random() * tetrominosArray.length);
         return randomIndex;
     }
 
 
     function moveDown(tetromino, grid, width, offset) {
-
         grid.value = undrawTetromino(tetromino, grid.value, offset);
-        // let movedTetromino = tetromino.map(index => index + width);
         offset += width;
         grid.value = drawTetromino(tetromino, grid.value, offset);
         return offset;
@@ -205,7 +233,6 @@
             currentTetromino.value = tetrominosArray[randomIndex.value][currentRotationIndex.value];
             offset.value = 4;
         }
-    
             offset.value = moveDown(currentTetromino.value, gridArray, width, offset.value);
         }, 500);
 }
